62d7b5823f88d5a47ab1ad8d14f283dfa47a086b
cloteau camille <camille.cloteau@gmail.com>
2022-03-22 11:32:25 +0100

    routes done

--- config/routes.rb
+++ config/routes.rb
@@ -2,11 +2,12 @@ Rails.application.routes.draw do
   devise_for :users
   root to: 'pages#home'
 
-  resources :pets do
+  resources :pets, only: %i[index show new create edit update] do
     scope module: :pets do
-      resources :types, only: %i[] do
-
-      end
+      resources :alerts, only: %i[index]
     end
   end
+
+  resources :pets, only: %i[destroy]
+  resources :vetos, only: %i[index]
 end
